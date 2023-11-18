47e6b128ed3307a0ebfb3a724fcf149ea27547a6
Kayhan <Kayhan@ulu.de>
2023-11-18 17:30:11 +0100

    Modified controller a bit

--- myCoolApp/src/main/java/com/springBootClass/myCoolApp/rest/FunRestController.java
+++ myCoolApp/src/main/java/com/springBootClass/myCoolApp/rest/FunRestController.java
@@ -1,13 +1,17 @@
 package com.springBootClass.myCoolApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    @Value("${developer.name}") //defined in application.resources
+    private String devName;
+
     @GetMapping("/")
     public String sayHell() {
-        return "Hello World!";
+        return "Hello " + devName + "!";
     }
 }
