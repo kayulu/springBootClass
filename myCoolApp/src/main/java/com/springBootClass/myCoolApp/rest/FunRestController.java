2b4febc041d180ef160bf2ba2abf2f01a02d24a3
Kayhan <Kayhan@ulu.de>
2023-11-18 15:58:24 +0100

    Add simple RestController

---
+++ myCoolApp/src/main/java/com/springBootClass/myCoolApp/rest/FunRestController.java
@@ -0,0 +1,13 @@
+package com.springBootClass.myCoolApp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHell() {
+        return "Hello World!";
+    }
+}
