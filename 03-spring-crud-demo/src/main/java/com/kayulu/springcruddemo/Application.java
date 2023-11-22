25df1e2a6e708e78568107c7891bb8fa89b31195
Kayhan <Kayhan@ulu.de>
2023-11-22 22:08:27 +0100

    Implement C of CRUD example

--- 03-spring-crud-demo/src/main/java/com/kayulu/springcruddemo/Application.java
+++ 03-spring-crud-demo/src/main/java/com/kayulu/springcruddemo/Application.java
@@ -1,5 +1,7 @@
 package com.kayulu.springcruddemo;
 
+import com.kayulu.springcruddemo.dao.StudentDAO;
+import com.kayulu.springcruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,15 +9,23 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDao) {
 		return runner -> {
-			System.out.println("Hello World!");
+			createStudent(studentDao);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDao) {
+		Student student = new Student();
+		student.setFirstName("Kay");
+		student.setLastName("Ulu");
+		student.setEmail("kay@ulu.de");
+
+		studentDao.save(student);
 	}
 }
---
+++ 03-spring-crud-demo/src/main/java/com/kayulu/springcruddemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.kayulu.springcruddemo.dao;
+
+import com.kayulu.springcruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student student);
+}
