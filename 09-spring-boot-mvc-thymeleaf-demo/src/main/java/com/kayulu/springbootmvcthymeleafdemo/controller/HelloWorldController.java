e8e0c34244550571283d7377d64904a9785843f9
Kayhan <Kayhan@ulu.de>
2023-12-26 23:37:13 +0100

    Demo adding request data to model

--- 09-spring-boot-mvc-thymeleaf-demo/src/main/java/com/kayulu/springbootmvcthymeleafdemo/controller/HelloWorldController.java
+++ 09-spring-boot-mvc-thymeleaf-demo/src/main/java/com/kayulu/springbootmvcthymeleafdemo/controller/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.kayulu.springbootmvcthymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
@@ -17,6 +19,18 @@ public class HelloWorldController {
 
     @GetMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @GetMapping("/processFormVersionTwo")
+    public String shoutUp(HttpServletRequest request, Model model) {
+        String studentName = request.getParameter("studentName");
+        studentName = studentName.toUpperCase();
+
+        String message = "Yo! " + studentName;
+
+        model.addAttribute("message", message);
+
         return "helloworld";
     }
 
