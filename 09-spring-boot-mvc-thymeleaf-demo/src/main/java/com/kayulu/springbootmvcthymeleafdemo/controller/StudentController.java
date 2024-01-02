f780bd8120271c6a5551e4369b6b2ed73543b7fe
Kayhan <Kayhan@ulu.de>
2024-01-02 14:09:57 +0100

    Use properties file to provide form data

--- 09-spring-boot-mvc-thymeleaf-demo/src/main/java/com/kayulu/springbootmvcthymeleafdemo/controller/StudentController.java
+++ 09-spring-boot-mvc-thymeleaf-demo/src/main/java/com/kayulu/springbootmvcthymeleafdemo/controller/StudentController.java
@@ -1,19 +1,24 @@
 package com.kayulu.springbootmvcthymeleafdemo.controller;
 
 import com.kayulu.springbootmvcthymeleafdemo.model.Student;
-import org.springframework.boot.Banner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    List<String> countries;
     @GetMapping("/studentForm")
     public String getStudentForm(Model model) {
         Student student = new Student();
         model.addAttribute("student", student);
+        model.addAttribute("countries", countries);
 
         return "student-form";
     }
@@ -25,6 +30,7 @@ public class StudentController {
 
         System.out.println("First Name: " + student.getFirstName());
         System.out.println("Last Name: " + student.getLastName());
+        System.out.println("Country: " + student.getCountry());
 
         return "studentInfo";
     }
