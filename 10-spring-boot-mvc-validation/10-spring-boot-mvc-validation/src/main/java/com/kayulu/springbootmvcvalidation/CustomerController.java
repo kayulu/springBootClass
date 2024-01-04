3e7f536e13aa5be8494c89e6d14e2031ce076e6e
Kayhan <Kayhan@ulu.de>
2024-01-04 13:51:52 +0100

    Demo InitBinder annotation and WebDataBinder
    Demo how a property editor can be used to trim white spaces of incoming web data strings

--- 10-spring-boot-mvc-validation/10-spring-boot-mvc-validation/src/main/java/com/kayulu/springbootmvcvalidation/CustomerController.java
+++ 10-spring-boot-mvc-validation/10-spring-boot-mvc-validation/src/main/java/com/kayulu/springbootmvcvalidation/CustomerController.java
@@ -1,17 +1,36 @@
 package com.kayulu.springbootmvcvalidation;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // Notes: @InitBinder allows to initialize WebDataBinder instances
+    // A WebDataBinder is responsible for data binding of web request data and Java objects.
+    // A WebDataBinder allows to configure data binding rules, register custom property editors and
+    // validators. That way incoming web data can be converted to Java objects or validated before it
+    // gets added to the model.
+    // A custom property editor is a class that can be used to convert a String in a specific format to a
+    // Java object. E.g. when a date string in the format 'yyyy-MM-dd' needs to be converted to a Date object.
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        // this is a predefined editor provided by Spring to trim white-spaces of a String.
+        // Note that this editor will process all incoming form fields
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @GetMapping("/")
-    private String showForm(Model model) {
+    public String showForm(Model model) {
         model.addAttribute("customer", new Customer());
 
         return "customer-form";
@@ -25,7 +44,7 @@ public class CustomerController {
     // the BindingResult object will also be added to the model and therefore will be
     // available at the view to check for validation errors.
     @PostMapping("/processForm")
-    private String processForm(
+    public String processForm(
             @Valid
             @ModelAttribute("customer") Customer customer,
             BindingResult bindingResult) {
