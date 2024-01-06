f63cdfc80085e60ff1fd45b657de820873957dd7
Kayhan <Kayhan@ulu.de>
2024-01-06 20:04:20 +0100

    Demo simple value range validation

--- 10-spring-boot-mvc-validation/10-spring-boot-mvc-validation/src/main/java/com/kayulu/springbootmvcvalidation/Customer.java
+++ 10-spring-boot-mvc-validation/10-spring-boot-mvc-validation/src/main/java/com/kayulu/springbootmvcvalidation/Customer.java
@@ -1,5 +1,7 @@
 package com.kayulu.springbootmvcvalidation;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,10 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "must have at least one character")
     private String lastName;
+
+    @Min(value = 1, message = "Value must be greater than or equal to 1")
+    @Max(value = 5, message = "Value must be less than or equal to 5")
+    private int freePasses;
 
     public String getFirstName() {
         return firstName;
@@ -24,5 +30,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
