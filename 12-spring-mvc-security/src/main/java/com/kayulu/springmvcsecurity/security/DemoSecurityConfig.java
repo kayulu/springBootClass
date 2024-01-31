d597fc9f0eaa483d8ee5382b86021c719f842e2b
Kayhan <Kayhan@ulu.de>
2024-01-31 14:00:35 +0100

    Adds custom login page
    Configures HttpSecurity for custom login page

--- 12-spring-mvc-security/src/main/java/com/kayulu/springmvcsecurity/security/DemoSecurityConfig.java
+++ 12-spring-mvc-security/src/main/java/com/kayulu/springmvcsecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.kayulu.springmvcsecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -30,5 +32,18 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/showLoginPage")
+                        .loginProcessingUrl("/authenticateUser")
+                        .permitAll() //allow everyone to access the login page without being logged in
+                );
+
+        return http.build();
     }
 }
