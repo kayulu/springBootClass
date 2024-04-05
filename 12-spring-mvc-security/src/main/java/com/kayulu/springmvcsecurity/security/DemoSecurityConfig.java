bbbacb6406b8d23ea2011ac68d0220b41484a70d
Kayhan <Kayhan@ulu.de>
2024-04-05 11:18:47 +0200

    Enables basic security using mysql database for user and password storage

--- 12-spring-mvc-security/src/main/java/com/kayulu/springmvcsecurity/security/DemoSecurityConfig.java
+++ 12-spring-mvc-security/src/main/java/com/kayulu/springmvcsecurity/security/DemoSecurityConfig.java
@@ -4,35 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-
     @Bean
-    InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    UserDetailsManager userDetailsManager(DataSource dataSource) { // DataSource will be auto-injected by Spring
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
